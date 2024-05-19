408fa6abdd8ac006e9af342ce113aab3a383862a
Calvine Otieno <nyarangaotieno@gmail.com>
2024-05-19 22:47:53 +0300

    chore: cleanup provider config
    Signed-off-by: Calvine Otieno <nyarangaotieno@gmail.com>

--- providers.tf
+++ providers.tf
@@ -1,20 +1,9 @@
-# provider "helm" {
-#   kubernetes {
-#     host                   = "https://${google_container_cluster._.endpoint}"
-# #    config_path    = "~/.kube/config"
-
-#     client_certificate     = base64decode(google_container_cluster._.master_auth.0.client_certificate)
-#     client_key             = base64decode(google_container_cluster._.master_auth.0.client_key)
-#     cluster_ca_certificate = base64decode(google_container_cluster._.master_auth.0.cluster_ca_certificate)
-#   }
-# }
-
 data "google_client_config" "default" {
 }
 provider "helm" {
   kubernetes {
-    host                   = "https://${google_container_cluster._.endpoint}/"
-    cluster_ca_certificate = base64decode(google_container_cluster._.master_auth.0.cluster_ca_certificate)
+    host                   = "https://${google_container_cluster.demo_cluster.endpoint}/"
+    cluster_ca_certificate = base64decode(google_container_cluster.demo_cluster.master_auth.0.cluster_ca_certificate)
     token                  = data.google_client_config.default.access_token
     #    client_key             = module.prodk8s.cluster_key
     #    client_certificate     = module.prodk8s.cluster_client_cert
@@ -22,16 +11,16 @@ provider "helm" {
 }
 
 provider "kubernetes" {
-  host                   = "https://${google_container_cluster._.endpoint}/"
-  cluster_ca_certificate = base64decode(google_container_cluster._.master_auth.0.cluster_ca_certificate)
+  host                   = "https://${google_container_cluster.demo_cluster.endpoint}/"
+  cluster_ca_certificate = base64decode(google_container_cluster.demo_cluster.master_auth.0.cluster_ca_certificate)
   token                  = data.google_client_config.default.access_token
   #  client_key             = module.prodk8s.cluster_key
   #  client_certificate     = module.prodk8s.cluster_client_cert
 }
 
 provider "kubectl" {
-  host                   = google_container_cluster._.endpoint
-  cluster_ca_certificate = base64decode(google_container_cluster._.master_auth.0.cluster_ca_certificate)
+  host                   = google_container_cluster.demo_cluster.endpoint
+  cluster_ca_certificate = base64decode(google_container_cluster.demo_cluster.master_auth.0.cluster_ca_certificate)
   token                  = data.google_client_config.default.access_token
   load_config_file       = false
 }
