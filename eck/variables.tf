ee9e55ff0f462da612eb5ef19608c75ccc0e89d1
Calvine Otieno <nyarangaotieno@gmail.com>
2024-07-02 08:36:09 +0300

    chore: cleanup the code
    Signed-off-by: Calvine Otieno <nyarangaotieno@gmail.com>

--- eck/variables.tf
+++ eck/variables.tf
@@ -1,40 +1,47 @@
-locals {
-  region = "europe-west4"
-}
-
-variable "kubernetes_name" {
+variable "cluster_name" {
   type        = string
   description = "GKE cluster name."
+  default = "demo"
 }
 
 variable "email" {
   type        = string
   description = "Please, enter your email (elastic email) or a user."
+  default = "calvineotieno.com"
 }
 
 variable "operator_version" {
   default = "2.12.1"
+  description = "The version of the eck to run."
 }
 
 variable "node_selector" {
   default = "elk"
+  description = "The node selector to use."
 }
 
 variable "gcp_project_id" {
-
+  description = "GCP project ID"
+  default = "your_example_project"
 }
 
 variable "clusterName" {
   default = "demo"
+  description = "ECK name"
 }
 
 variable "elastic_user_password" {
+  default = "ZKzPgIKt3lp5EG8JtuS8KzJFOUM"
+  sensitive = true
+  description = "ELK user custom password."
 }
 
-variable "host" {
-
+variable "fqdn" {
+  description = "The FQDN to use for Elastic and Kibana. example calvineotieno.com -> kibana.calvineotieno.com."
+  default = "calvineotieno.com"
 }
 
 variable "HostEmail" {
-
+  default = "calvineotieno.com"
+  description = "The email address to use for managing LetsEncypt certs by Cert Manager."
 }
